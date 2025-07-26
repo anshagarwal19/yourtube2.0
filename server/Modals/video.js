b583f0aedda66b2d6c1e84e63e236b39bdaffba4
anshagarwal19 <agarwalansh.1421@gmail.com>
2025-07-26 17:58:39 +0530

    video modal

--- server/Modals/video.js
+++ server/Modals/video.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
+const resolutionSchema = new mongoose.Schema({
+  resolution: {type:String, required: true,default: "720p"},
+  path: {type:String, required: true,default: "720p.mp4"},
+});
 const videochema = new mongoose.Schema(
   {
     videotitle: { type: String, required: true },
@@ -7,12 +11,7 @@ const videochema = new mongoose.Schema(
     filetype: { type: String, required: true },
     filepath: { type: String, required: true },
     filesize: { type: String, required: true },
-    resolutions: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Resolution",
-      },
-    ],
+    resolutions: [resolutionSchema],
     videochanel: { type: String, required: true },
     Like: { type: Number, default: 0 },
     views: { type: Number, default: 0 },
